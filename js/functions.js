4bb8ca967d50d305c53fbb4556b9169afcd8098b
Ray <rafaelgame2012@gmail.com>
2020-12-27 20:22:08 -0600

    Primer commit, implementación del HTML y funciones básicas de JS

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+'use strict'
+//De esta manera puedes enlazar funciones con etiquetas
+function alertar(){
+    var alerta = document.getElementById("home");
+    window.open("about.html");
+}
+/* Haz abierto TODO un mundo de posibilidades*/
